48ccae893f4c9f2dab75f864912b1c92c4088d08
Gerald <darkobekoegerald2@gmail.com>
2024-11-05 17:24:39 +0000

    Made some minor changes

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,38 +1,62 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styles from "./SignInStyling/signin.module.css"
 import Intro from './Intro';
 import AccountCreation from './AccountCreation';
 import AccountLogin from './AccountLogin';
+import { useNavigate } from 'react-router-dom';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 export default function Signin() {
-  const [showIntro, setShowIntro] = useState(true);
+  const [showIntro, setShowIntro] = useState(false);
 
   const [showSignup, setShowSignup] = useState(false);
 
-  const [showSignin, setShowSignin] = useState(false);
+  const [showSignin, setShowSignin] = useState(true);
 
   const [isHostel, setIsHostel] = useState(false);
 
   const sliderRef = useRef();
 
+  const navigate = useNavigate();
+
+  const onBackButtonEvent = (e) =>{
+    if(showSignin){
+      navigate("/");
+    }else{
+      navigate("/signin");
+      if(showSignup){
+        displayIntroPage()
+      }else{
+        displaySigninPage()
+      }
+    }
+  }
+
+  useEffect(() => {
+    navigate("/signin");
+    window.addEventListener('popstate', onBackButtonEvent);
+    return () => {
+      window.removeEventListener('popstate', onBackButtonEvent);  
+    };
+  }, [showIntro]); // eslint-disable-line react-hooks/exhaustive-deps
+
   const displayIntroPage = () =>{
-    sliderRef.current.style.right = `0`;
+    sliderRef.current.style.right = `100%`;
     setShowSignup(false);
     setShowSignin(false);
     setShowIntro(true);
   }
 
   const displaySignupPage = () =>{
-    sliderRef.current.style.right = `100%`;
+    sliderRef.current.style.right = `200%`;
     setShowIntro(false);
     setShowSignin(false);
     setShowSignup(true);
   }
 
   const displaySigninPage = () =>{
-    sliderRef.current.style.right = `200%`;
+    sliderRef.current.style.right = `0`;
     setShowIntro(false);
     setShowSignin(true);
     setShowSignup(false);
@@ -56,9 +80,9 @@ export default function Signin() {
                 <p>Gerald Caroline- Fresh Graduate of Adamo University</p>
 
                 <div className={styles.login_steps_container}>
+                <div className={`${styles.step} ${showSignin && styles.active}`}></div>
                     <div className={`${styles.step} ${showIntro && styles.active}`}></div>
                     <div className={`${styles.step} ${showSignup && styles.active}`}></div>
-                    <div className={`${styles.step} ${showSignin && styles.active}`}></div>
                 </div>
             </div>
       </div>
